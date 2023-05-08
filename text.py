2bff17a68c0fb1b5c960f429c98c56b8953f65ba
DOVAGU <2220988763@QQ.COM>
2023-05-08 11:45:50 +0800

    finished w8

--- text.py
+++ text.py
@@ -1,6 +1,10 @@
-word_to_count = {}
-for word in words:
-    if word in word_to_count:
-        word_to_count[word] += 1
-    else:
-        word_to_count[word] = 1
+import kivy
+from kivy.app import App
+from kivy.uix.button import Button
+
+class MyApp(App):
+    def build(self):
+        return Button(text='Hello Kivy')
+
+if __name__ == '__main__':
+    MyApp().run()
